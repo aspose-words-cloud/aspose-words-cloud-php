c127a79a3f4270da481ba8da1cb737b7fcd70bd5
yaroslaw.ekimov <yaroslaw.ekimov@aspose.com>
2020-01-08 22:25:54 -0800

    SDK regenerated by CI server [ci skip]

--- src/Aspose/Words/Configuration.php
+++ src/Aspose/Words/Configuration.php
@@ -511,7 +511,7 @@ class Configuration
         $report  = 'PHP SDK (Aspose\Words) Debug Report:' . PHP_EOL;
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
-        $report .= '    OpenAPI Spec Version: 19.11.0' . PHP_EOL;
+        $report .= '    OpenAPI Spec Version: 19.12.0' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;

24db288c3784c0140b825b78eca8250e499a4e23
wordscloud <aspose.cloud@aspose.com>
2020-02-20 23:24:08 -0800

    SDK regenerated by CI server [ci skip]

--- src/Aspose/Words/Model/FootnotesResponse.php
+++ src/Aspose/Words/Model/FootnotesResponse.php
@@ -35,7 +35,7 @@ use \Aspose\Words\ObjectSerializer;
 /*
  * FootnotesResponse
  *
- * @description This response should be returned by the service when handling: GET http://api.aspose.com/v4.0/words/Test.doc/footnotes.
+ * @description This response should be returned by the service when handling: GET https://api.aspose.cloud/v4.0/words/Test.doc/footnotes.
  */
 class FootnotesResponse extends WordsResponse 
 {

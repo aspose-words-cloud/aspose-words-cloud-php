7c1603f13e9a77c3e776e115d7484717b4cb89a6
wordscloud <aspose.cloud@aspose.com>
2020-03-24 01:35:34 -0700

    SDK regenerated by CI server [ci skip]

--- src/Aspose/Words/WordsApi.php
+++ src/Aspose/Words/WordsApi.php
@@ -68,11 +68,11 @@ class WordsApi
     public function __construct(string $appSid, string $appKey, string $baseUrl)
     {
         if (!isset($appSid) || trim($appSid) === '') {
-            throw new ApiException('appSid could not be an empty string.')
+            throw new ApiException('appSid could not be an empty string.');
         }
         
         if (!isset($appKey) || trim($appKey) === '') {
-            throw new ApiException('appKey could not be an empty string.')
+            throw new ApiException('appKey could not be an empty string.');
         }
         
         $this->client = new Client();

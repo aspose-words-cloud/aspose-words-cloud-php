4a1f7338df7d16a70ab17611ae34a8e3fffa23e2
jenkins <jenkins.aspose@gmail.com>
2021-10-07 05:30:56 -0500

    SDK regenerated by CI server [ci skip]

--- src/Aspose/Words/Configuration.php
+++ src/Aspose/Words/Configuration.php
@@ -134,7 +134,7 @@ class Configuration
      * Version of Aspose.Words Cloud API
      *
      */
-    protected $clientVersion = '21.9';
+    protected $clientVersion = '21.10';
 
     /*
      * Constructor
@@ -516,7 +516,7 @@ class Configuration
         $report  = 'PHP SDK (Aspose\Words) Debug Report:' . PHP_EOL;
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
-        $report .= '    OpenAPI Spec Version: 21.9' . PHP_EOL;
+        $report .= '    OpenAPI Spec Version: 21.10' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;

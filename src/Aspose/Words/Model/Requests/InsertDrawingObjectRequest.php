fd13c0da69609cc084408304cd13bffd8f96ed6d
Dmitry Finozhenok <dmitry.finozhenok@aspose.com>
2022-01-28 14:14:50 +0900

    test temlates

--- src/Aspose/Words/Model/Requests/InsertDrawingObjectRequest.php
+++ src/Aspose/Words/Model/Requests/InsertDrawingObjectRequest.php
@@ -37,7 +37,7 @@ use GuzzleHttp\RequestOptions;
 use Aspose\Words\ObjectSerializer;
 use Aspose\Words\HeaderSelector;
 use Aspose\Words\Model\Response\InsertDrawingObjectResponse;
-use phpseclib3\Crypt\RSA;
+use Aspose\Words\Encryptor;
 
 /*
  * Request model for insertDrawingObject operation.
@@ -468,8 +468,7 @@ class InsertDrawingObjectRequest extends BaseApiRequest
         if (property_exists($this, 'password') && $this->password != null)
         {
             unset($queryParams['password']);
-            $pub = RSA::loadPublicKey($config->getRsaKey());
-            $queryParams['encryptedPassword'] = base64_encode($pub->withPadding(RSA::ENCRYPTION_PKCS1)->encrypt($this->password));
+            $queryParams['encryptedPassword'] = $config->getEncryptor()->encrypt($this->password);
         }
 
         $resourcePath = ObjectSerializer::parseURL($config, $resourcePath, $queryParams);
--- src/Aspose/Words/Model/Requests/RenderTableRequest.php
+++ src/Aspose/Words/Model/Requests/RenderTableRequest.php
@@ -37,7 +37,7 @@ use GuzzleHttp\RequestOptions;
 use Aspose\Words\ObjectSerializer;
 use Aspose\Words\HeaderSelector;
 use Aspose\Words\Model\Response\RenderTableResponse;
-use phpseclib3\Crypt\RSA;
+use Aspose\Words\Encryptor;
 
 /*
  * Request model for renderTable operation.
@@ -453,8 +453,7 @@ class RenderTableRequest extends BaseApiRequest
         if (property_exists($this, 'password') && $this->password != null)
         {
             unset($queryParams['password']);
-            $pub = RSA::loadPublicKey($config->getRsaKey());
-            $queryParams['encryptedPassword'] = base64_encode($pub->withPadding(RSA::ENCRYPTION_PKCS1)->encrypt($this->password));
+            $queryParams['encryptedPassword'] = $config->getEncryptor()->encrypt($this->password);
         }
 
         $resourcePath = ObjectSerializer::parseURL($config, $resourcePath, $queryParams);

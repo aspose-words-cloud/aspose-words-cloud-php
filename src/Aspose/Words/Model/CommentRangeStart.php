55cf39d83832edee7cef08f77fced2546df7f413
jenkins <jenkins.aspose@gmail.com>
2025-04-22 13:11:31 -0500

    SDK regenerated by CI server [ci skip]

---
+++ src/Aspose/Words/Model/CommentRangeStart.php
@@ -0,0 +1,289 @@
+<?php
+/*
+ * --------------------------------------------------------------------------------
+ * <copyright company="Aspose" file="CommentRangeStart.php">
+ *   Copyright (c) 2025 Aspose.Words for Cloud
+ * </copyright>
+ * <summary>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ * </summary>
+ * --------------------------------------------------------------------------------
+ */
+
+namespace Aspose\Words\Model;
+use \Aspose\Words\ObjectSerializer;
+
+/*
+ * CommentRangeStart
+ *
+ * @description Comment range start link.
+ */
+class CommentRangeStart extends NodeLink
+{
+    const DISCRIMINATOR = null;
+
+    /*
+     * The original name of the model.
+     *
+     * @var string
+     */
+    protected static $swaggerModelName = "CommentRangeStart";
+
+    /*
+     * Array of property to type mappings. Used for (de)serialization
+     *
+     * @var string[]
+     */
+    protected static $swaggerTypes = [
+        'comment_link' => '\Aspose\Words\Model\CommentLink'
+    ];
+
+    /*
+     * Array of property to format mappings. Used for (de)serialization
+     *
+     * @var string[]
+     */
+    protected static $swaggerFormats = [
+        'comment_link' => 'null'
+    ];
+
+    /*
+     * Array of property to type mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function swaggerTypes()
+    {
+        return self::$swaggerTypes + parent::swaggerTypes();
+    }
+
+    /*
+     * Array of property to format mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function swaggerFormats()
+    {
+        return self::$swaggerFormats + parent::swaggerFormats();
+    }
+
+    /*
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @var string[]
+     */
+    protected static $attributeMap = [
+        'comment_link' => 'CommentLink'
+    ];
+
+    /*
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @var string[]
+     */
+    protected static $setters = [
+        'comment_link' => 'setCommentLink'
+    ];
+
+    /*
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @var string[]
+     */
+    protected static $getters = [
+        'comment_link' => 'getCommentLink'
+    ];
+
+    /*
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @return array
+     */
+    public static function attributeMap()
+    {
+        return parent::attributeMap() + self::$attributeMap;
+    }
+
+    /*
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @return array
+     */
+    public static function setters()
+    {
+        return parent::setters() + self::$setters;
+    }
+
+    /*
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @return array
+     */
+    public static function getters()
+    {
+        return parent::getters() + self::$getters;
+    }
+
+    /*
+     * The original name of the model.
+     *
+     * @return string
+     */
+    public function getModelName()
+    {
+        return self::$swaggerModelName;
+    }
+
+
+
+    /*
+     * Constructor
+     *
+     * @param mixed[] $data Associated array of property values
+     *                      initializing the model
+     */
+    public function __construct(array $data = null)
+    {
+        parent::__construct($data);
+        $this->container['comment_link'] = isset($data['comment_link']) ? $data['comment_link'] : null;
+    }
+
+    /*
+     * Validate all the properties in the model
+     */
+    public function validate()
+    {
+        parent::validate();
+
+
+        if (isset($this->container['comment_link'])) {
+            $this->getCommentLink()->validate();
+        }
+
+    }
+
+    /*
+     * Gets comment_link
+     *
+     * @return \Aspose\Words\Model\CommentLink
+     */
+    public function getCommentLink()
+    {
+        return $this->container['comment_link'];
+    }
+
+    /*
+     * Sets comment_link
+     *
+     * @param \Aspose\Words\Model\CommentLink $comment_link Gets or sets the link to comment.
+     *
+     * @return $this
+     */
+    public function setCommentLink($comment_link)
+    {
+        $this->container['comment_link'] = $comment_link;
+        return $this;
+    }
+
+
+    /*
+     * Returns true if offset exists. False otherwise.
+     *
+     * @param integer $offset Offset
+     *
+     * @return boolean
+     */
+	#[\ReturnTypeWillChange]
+    public function offsetExists($offset)
+    {
+        return isset($this->container[$offset]);
+    }
+
+    /*
+     * Gets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return mixed
+     */
+	#[\ReturnTypeWillChange]
+    public function offsetGet($offset)
+    {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+
+    /*
+     * Sets value based on offset.
+     *
+     * @param integer $offset Offset
+     * @param mixed   $value  Value to be set
+     *
+     * @return void
+     */
+	#[\ReturnTypeWillChange]
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+
+    /*
+     * Unsets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return void
+     */
+	#[\ReturnTypeWillChange]
+    public function offsetUnset($offset)
+    {
+        unset($this->container[$offset]);
+    }
+
+    /*
+     * Collect all FileReference variables to list
+     */
+    public function collectFilesContent($resultFilesContent)
+    {
+        return $resultFilesContent;
+    }
+
+    /*
+     * Gets the string presentation of the object
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        if (defined('JSON_PRETTY_PRINT')) { // use JSON pretty print
+            return json_encode(
+                ObjectSerializer::sanitizeForSerialization($this),
+                JSON_PRETTY_PRINT
+            );
+        }
+
+        return json_encode(ObjectSerializer::sanitizeForSerialization($this));
+    }
+}
+

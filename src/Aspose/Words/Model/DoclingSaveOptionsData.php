2373cc23b099d5157ba92233f15b7246a49fb4f0
jenkins <jenkins.aspose@gmail.com>
2026-02-02 08:30:32 -0600

    SDK regenerated by CI server [ci skip]

---
+++ src/Aspose/Words/Model/DoclingSaveOptionsData.php
@@ -0,0 +1,285 @@
+<?php
+/*
+ * --------------------------------------------------------------------------------
+ * <copyright company="Aspose" file="DoclingSaveOptionsData.php">
+ *   Copyright (c) 2026 Aspose.Words for Cloud
+ * </copyright>
+ * <summary>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ * </summary>
+ * --------------------------------------------------------------------------------
+ */
+
+namespace Aspose\Words\Model;
+use \Aspose\Words\ObjectSerializer;
+
+/*
+ * DoclingSaveOptionsData
+ *
+ * @description Container class for docling json save options.
+ */
+class DoclingSaveOptionsData extends SaveOptionsData
+{
+    const DISCRIMINATOR = null;
+
+    /*
+     * The original name of the model.
+     *
+     * @var string
+     */
+    protected static $swaggerModelName = "DoclingSaveOptionsData";
+
+    /*
+     * Array of property to type mappings. Used for (de)serialization
+     *
+     * @var string[]
+     */
+    protected static $swaggerTypes = [
+        'render_non_image_shapes' => 'bool'
+    ];
+
+    /*
+     * Array of property to format mappings. Used for (de)serialization
+     *
+     * @var string[]
+     */
+    protected static $swaggerFormats = [
+        'render_non_image_shapes' => 'null'
+    ];
+
+    /*
+     * Array of property to type mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function swaggerTypes()
+    {
+        return self::$swaggerTypes + parent::swaggerTypes();
+    }
+
+    /*
+     * Array of property to format mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function swaggerFormats()
+    {
+        return self::$swaggerFormats + parent::swaggerFormats();
+    }
+
+    /*
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @var string[]
+     */
+    protected static $attributeMap = [
+        'render_non_image_shapes' => 'RenderNonImageShapes'
+    ];
+
+    /*
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @var string[]
+     */
+    protected static $setters = [
+        'render_non_image_shapes' => 'setRenderNonImageShapes'
+    ];
+
+    /*
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @var string[]
+     */
+    protected static $getters = [
+        'render_non_image_shapes' => 'getRenderNonImageShapes'
+    ];
+
+    /*
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @return array
+     */
+    public static function attributeMap()
+    {
+        return parent::attributeMap() + self::$attributeMap;
+    }
+
+    /*
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @return array
+     */
+    public static function setters()
+    {
+        return parent::setters() + self::$setters;
+    }
+
+    /*
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @return array
+     */
+    public static function getters()
+    {
+        return parent::getters() + self::$getters;
+    }
+
+    /*
+     * The original name of the model.
+     *
+     * @return string
+     */
+    public function getModelName()
+    {
+        return self::$swaggerModelName;
+    }
+
+
+
+    /*
+     * Constructor
+     *
+     * @param mixed[] $data Associated array of property values
+     *                      initializing the model
+     */
+    public function __construct(array $data = null)
+    {
+        parent::__construct($data);
+        $this->container['render_non_image_shapes'] = isset($data['render_non_image_shapes']) ? $data['render_non_image_shapes'] : null;
+        $this->container['save_format'] = "docling";
+    }
+
+    /*
+     * Validate all the properties in the model
+     */
+    public function validate()
+    {
+        parent::validate();
+
+    }
+
+    /*
+     * Gets render_non_image_shapes
+     *
+     * @return bool
+     */
+    public function getRenderNonImageShapes()
+    {
+        return $this->container['render_non_image_shapes'];
+    }
+
+    /*
+     * Sets render_non_image_shapes
+     *
+     * @param bool $render_non_image_shapes Gets or sets a value indicating whether non-image shapes should be rendered and written to the output Docling JSON document. If the property is false, non-image shapes are not exported to the output document. The default value is false.
+     *
+     * @return $this
+     */
+    public function setRenderNonImageShapes($render_non_image_shapes)
+    {
+        $this->container['render_non_image_shapes'] = $render_non_image_shapes;
+        return $this;
+    }
+
+
+    /*
+     * Returns true if offset exists. False otherwise.
+     *
+     * @param integer $offset Offset
+     *
+     * @return boolean
+     */
+	#[\ReturnTypeWillChange]
+    public function offsetExists($offset)
+    {
+        return isset($this->container[$offset]);
+    }
+
+    /*
+     * Gets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return mixed
+     */
+	#[\ReturnTypeWillChange]
+    public function offsetGet($offset)
+    {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+
+    /*
+     * Sets value based on offset.
+     *
+     * @param integer $offset Offset
+     * @param mixed   $value  Value to be set
+     *
+     * @return void
+     */
+	#[\ReturnTypeWillChange]
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+
+    /*
+     * Unsets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return void
+     */
+	#[\ReturnTypeWillChange]
+    public function offsetUnset($offset)
+    {
+        unset($this->container[$offset]);
+    }
+
+    /*
+     * Collect all FileReference variables to list
+     */
+    public function collectFilesContent($resultFilesContent)
+    {
+        return $resultFilesContent;
+    }
+
+    /*
+     * Gets the string presentation of the object
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        if (defined('JSON_PRETTY_PRINT')) { // use JSON pretty print
+            return json_encode(
+                ObjectSerializer::sanitizeForSerialization($this),
+                JSON_PRETTY_PRINT
+            );
+        }
+
+        return json_encode(ObjectSerializer::sanitizeForSerialization($this));
+    }
+}
+
